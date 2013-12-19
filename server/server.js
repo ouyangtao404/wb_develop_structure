8d2844d290a4cb8f75a45ae5d127003013968737
抱鹤 <baohe.oyt@alibaba-inc.com>
2013-12-19 16:46:32 +0800

    test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 /**
- * 服务器
+ * 服务启动文件
  */
 var ip = '127.0.0.1';
+var ip = "10.32.185.94";
 var port = 8888;
 var host = ip + ':' + port;
 
@@ -28,3 +29,23 @@ var less2css = require('./less2css');
     http.createServer(onRequest).listen(port);
     console.log('Server has started! path is '+ ip + ':' + port);
 })();
+
+
+var options = {
+    host: 'www.google.com', port: 80,
+    path: '/upload', method: 'POST'
+};
+var req = http.request(options, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res. headers));
+    res.setEncoding('utf8'); res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk); });
+});
+req.on('error', function (e) {
+    console.log('problem with request: ' + e.message);
+});
+// write data to request body req.write('data\n'); req.write('data\n'); req.end();
+值得一提的是如果对一个事件添加了超过10个侦听器,将会得到一条警告,
+    这一处设计与Node.js自身单线程运行有关,设计者认为侦听器太多,可能导致内存泄漏,所以存在这样一个警告。调用:
+    emitter.setMaxListeners(0);
+可以将这个限制去掉
